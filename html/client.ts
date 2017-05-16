42997183617e9e5bab5125a7d17aa0b8eaa72d64
Jared Perry <jared@perrycode.com>
2017-05-16 13:20:40 -0400

    typescript browserify etc etc

---
+++ html/client.ts
@@ -0,0 +1,7 @@
+import * as io from 'socket.io-client';
+
+const socket = io('http://localhost');
+socket.on('news', function (data) {
+    console.log(data);
+    socket.emit('my other event', { my: 'data' });
+});
