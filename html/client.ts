80ea86107e0ddc2385e91672dac8249235883008
Jared Perry <jared@perrycode.com>
2017-05-18 16:39:04 -0400

    excalibur test game

--- html/client.ts
+++ html/client.ts
@@ -1,5 +1,7 @@
 import * as io from 'socket.io-client';
 import Person from "../shared/Person";
+import * as ex from 'excalibur'
+import {PointerEvent} from "Input/Pointer";
 
 const socket = io('http://localhost');
 
@@ -7,3 +9,19 @@ socket.on('why dont you say hello', function() {
     const user: Person = { firstName: 'Billy', lastName: 'Bob' };
     socket.emit('give name', user);
 });
+
+const game = new ex.Engine({
+    width: 800,
+    height: 600,
+    canvasElementId: 'gameSurface'
+});
+
+const paddle = new ex.Actor(150, game.getDrawHeight() - 40, 200, 20);
+paddle.color = ex.Color.Chartreuse;
+paddle.collisionType = ex.CollisionType.Fixed;
+game.input.pointers.primary.on('move', function (evt: PointerEvent) {
+    paddle.pos.x = evt.x;
+});
+game.add(paddle);
+
+game.start();
